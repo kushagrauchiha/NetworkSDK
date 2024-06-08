a4a03633dcafc58c6e02779f16dd55b542d5ce6b
kushagrauchiha <mishraa.kushagra@gmail.com>
2024-06-09 03:10:47 +0530

    replacing with xcframework

---
+++ NetworkSDK.xcframework/ios-arm64_x86_64-maccatalyst/NetworkSDK.framework/Versions/A/Headers/NetworkSDK.h
@@ -0,0 +1,18 @@
+//
+//  NetworkSDK.h
+//  NetworkSDK
+//
+//  Created by Kushagra Mishra on 09/06/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NetworkSDK.
+FOUNDATION_EXPORT double NetworkSDKVersionNumber;
+
+//! Project version string for NetworkSDK.
+FOUNDATION_EXPORT const unsigned char NetworkSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetworkSDK/PublicHeader.h>
+
+
